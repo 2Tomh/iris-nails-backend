3886d4c32dcfd2853e2213ee1b4bc3727b42b4ca
Tom <2tom.hayoun@gmail.com>
2023-06-12 12:48:17 +0300

    added pages

--- routers/purchase.js
+++ routers/purchase.js
@@ -1,21 +1,23 @@
 const express= require('express');
 const Purchase = require("../schemas/purchaseSchema");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const {verifyToken, verifyAdmin} = require("../utils/auth");
 
-router.post("/purchase", async(req, res) => {
+router.post("/purchase", verifyToken, verifyAdmin ,async(req, res) => {
+    
     if(!req.body.quantity || !req.body.productId){
         return res.status(403).send("All fields are required");
     }
     const purchase = new Purchase({quantity:req.body.quantity, product: req.body.productId});
-    
+
     await purchase.save();
     res.send(purchase);
 })
 
 
-router.get("/purchases", async(req, res)=>{
-    const purchases = await Purchase.find().populate("product");;
-
+router.get("/purchases",verifyToken, verifyAdmin, async(req, res)=>{
+    const purchases = await Purchase.find({}).populate("product");;
     res.send(purchases);
     
 })
